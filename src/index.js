ab34d6febdc1c43b6cd551b0009f085d9c32d027
Tirthankar Roy <roytirthankar2@gmail.com>
2023-01-29 10:49:49 +0530

    final push

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 const express = require("express");
-const express = require("express");
-const route = require("./routes/route")
 const route = require("./routes/route");
-
 
 const {default:mongoose} = require("mongoose")
 
@@ -10,11 +7,11 @@ const app = express();
 
 app.use(express.json())
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb+srv://Tirtha008:mlpnk3AhOzHztAdO@tirthacluster.psqixlb.mongodb.net/Bonus5", {useNewUrlParser: true})
+mongoose.connect("My DB Link", {useNewUrlParser: true})
 .then(()=>console.log("MongoDB is Connected"))
 .catch(err=>console.log(err))
 app.use("/", route)
 
 app.listen(3000, function(){
-    console.log("Express App is running on port" + (3000))
+    console.log("Express App is running on port " + (3000))
 })
